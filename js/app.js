602270e768de9bfd1597b1079ed64a61e2266787
eugene <eugenecarbado@gmail.com>
2021-01-07 07:59:01 +0000

    .

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ document.addEventListener('scroll', function() {
     const topOfNav = navHeader.clientHeight;
 
     function changeNav() {
-        if(window.scrollY >= topOfNav) {
+        if(window.scrollY >= topOfNav - topOfNav + 1) {
             document.body.classList.add('sticky');
         } else {
             document.body.classList.remove('sticky');
